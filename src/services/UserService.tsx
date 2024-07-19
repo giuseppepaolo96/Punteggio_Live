d1361b6849dd6f57f5811836d35984301b3f81e6
giuseppepaolo96 <47814821+giuseppepaolo96@users.noreply.github.com>
2024-07-19 10:31:00 +0200

    Delete src directory

--- src/services/UserService.tsx
+++
@@ -1,7 +0,0 @@
-import { axiosInstance } from "utils";
-
-export const checkUsername = async (username: string) => {
-  return axiosInstance.get(
-    `auth/authentication/check-username?username=${username}`
-  );
-};
--- src/services/index.tsx
+++
@@ -1,1 +0,0 @@
-export { checkUsername } from "./UserService";
